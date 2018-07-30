76121c8f153c603636ce8391308e3db83d5bb843
Bruno Lemos <brunohplemos@gmail.com>
2018-07-30 18:23:43 -0300

    [TypeScript] Add images typescript definition

---
+++ packages/react-scripts/template/index.d.ts
@@ -0,0 +1,12 @@
+declare module '*.jpg'
+
+declare module '*.png'
+
+declare module '*.svg' {
+  import * as React from 'react'
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+
+  const src: string
+  export default src
+}
